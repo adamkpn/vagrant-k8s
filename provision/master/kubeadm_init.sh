4ae10f58adacc423ad8d1bd345aa4e27c2b6efa9
Adam Kaplun <adam.kaplun@crossix.veeva.com>
2020-01-27 20:05:58 +0200

    changed --pod-network-cidr=10.244.0.0/16

--- provision/master/kubeadm_init.sh
+++ provision/master/kubeadm_init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Initialize Kubernetes Cluster
-kubeadm init --ignore-preflight-errors=NumCPU --apiserver-advertise-address $(hostname -i) --pod-network-cidr=192.168.0.0/16 --token=9201e0.9c84a8ad258cf7ab
+kubeadm init --ignore-preflight-errors=NumCPU --apiserver-advertise-address $(hostname -i) --pod-network-cidr=10.244.0.0/16 --token=9201e0.9c84a8ad258cf7ab
